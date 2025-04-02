a3fff68ad4524c66621c3a5388f8dcdc340ca64a
Paolafigueroah <claudia.figueroa244277@potros.itson.edu.mx>
2025-04-02 09:24:43 -0700

    Creación de la API para la aplicacion de CheemsTour

---
+++ persistence/db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='120212paoFH',
+        database='cheemstour'
+    )
+
+    
